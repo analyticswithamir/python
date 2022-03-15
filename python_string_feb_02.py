9602e58698d6fa1ffb9d5fc1ed385f57d68c6b93
digitalreza9 <99802527+digitalreza9@users.noreply.github.com>
2022-03-15 07:32:46 -0700

    Add files via upload

---
+++ python_string_feb_02.py
@@ -0,0 +1,102 @@
+####PYTHON STRING
+
+##var = "dhoni"
+##print(var[0])
+
+## d = 0, -5
+## h = 1, -4
+## o = 2, -3
+## n = 3, -2
+## i = 4, -1
+
+##var = "dhoni"
+##print(var[4])
+
+##var = "dhoni"
+##print(var[-4])
+
+####var = "dhoni"
+####var[4] = "y"
+
+##var = "dhoni msd"
+##print(var[0])
+
+##print(var[0:])
+
+##print(var[0:6])
+
+##print(var[6:0])    # not valid slicing scenerios
+ 
+##var = "dhoni msd"
+##print(var[-1])
+
+##var = "dhoni msd"
+##print(var[-1:])      # string slicing always happen forward direction
+
+##var = "dhoni msd"
+##print(var[-2:])
+
+##var = "dhoni msd"
+##print(var[-2:])
+
+##var = "dhoni msd"
+##print(var[::2])
+
+##var = "dhoni msd"
+##print(var[::-2])
+
+##var = "dhoni msd"
+##print(var[::-1])     # string reversal
+
+##var = "dhoni"
+##var1 = "msd"
+##print(var + var1)
+
+##var = "dhoni"
+##var1 = 90
+##print(var +var1) @TypeError: can only concatenate str (not "int") to str
+
+##var = "dhoni"
+##var1 = 90
+##print(var +str(var1))
+
+##var = "dhoni","msd"
+##print(var)
+##print(type(var))
+
+
+##var,var1 = "dhoni","msd"
+##print(var)
+##print(var1)
+
+
+
+##name = "dhoni"
+##score = 183
+##statement = "captain scored against srilanka"
+##statement = "captain" + name + "scored"+ str(score) + "against srilanka"
+##print(statement)
+
+
+
+##satement = "captain scored against srilanka"(name,score)
+##print(statement)
+
+
+
+####satement = "captain{} scored{ } against srilanka", format (name,score)
+####print(statement)
+
+
+####satement = "captain{name} scored{score } against srilanka" 
+####print(statement)
+
+
+var = "'dhoni is my captain and/
+he plays for csk"'
+print(var)
+
+
+
+
+
